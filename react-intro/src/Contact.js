ea76d6bb36aca1ca9af0f79898c2eac3e45c2738
arabfysal <arabfysal@gmail.com>
2021-10-11 08:41:25 +0800

    Add files via upload

---
+++ react-intro/src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>phone: 08012345678</li>
+          <li>email: arabfysal@gmail.com</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ react-intro/src/Stuff.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>stuff</h2>
+        <p>other stuff</p>
+      </div>
+    );
+  }
+}
+
+export default Stuff;
